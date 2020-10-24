b6f025bd20654131a7ee7e6958d36a1016a43292
Marina <mika-klubnika@yandex.ru>
2020-10-24 20:58:50 +0300

    one1

--- js/js.js
+++ js/js.js
@@ -6,24 +6,24 @@ const departureInput = document.querySelector('.departure-input');
 
 button.onclick = function() {
   popup.classList.toggle('modal-not-show');
-  popup.classList.remove("modal-error");
+  popup.classList.remove('modal-error');
 }
 
-formSearch.addEventListener("submit", function (evt) {
+formSearch.addEventListener('submit', function (evt) {
   if (!arrivalInput.value || !departureInput.value) {
     evt.preventDefault();
-    popup.classList.remove("modal-error");
+    popup.classList.remove('modal-error');
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
+    popup.classList.add('modal-error');
   }
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
       evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-          popup.classList.add("modal-not-show");
-          popup.classList.remove("modal-error");
+      if (popup.classList.contains('modal-show')) {
+          popup.classList.add('modal-not-show');
+          popup.classList.remove('modal-error');
       }
   }
 });
